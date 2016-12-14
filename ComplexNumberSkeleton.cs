395b79449f4f1fedbb7b949dabeae51202004b64
bobbycarver <bobbycarver38@gmail.com>
2016-12-14 13:56:04 +0000

    Add files via upload

---
+++ ComplexNumberSkeleton.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ComplexNumberApplication
+{
+    class ComplexNumber
+    {
+        // instance variables
+        private double re;
+        private double im;
+
+        // initialise a new complex number to zero
+        public ComplexNumber()
+        {
+            this.re = 0.0;
+            this.im = 0.0;
+        }
+
+        // initialise a new complex number to be equal to
+        // specified complex number
+        public ComplexNumber(ComplexNumber other)
+        {
+            this.re = other.GetRe();
+            this.im = other.GetIm();
+        }
+
+        // initialise a new complex number to be equal to
+        // specified values
+        public ComplexNumber(double real, double imaginary)
+        {
+            // ** COMPLETE **
+        }
+
+        // return real part of complex number
+        public double GetRe()
+        {
+            return this.re;
+        }
+
+        // return imaginary part of complex number
+        public double GetIm()
+        {
+            return this.im;
+        }
+
+        // add specified complex number to current complex number
+        public void Add(ComplexNumber other)
+        {
+            // ** COMPLETE ** -- Requires use of GetRe(), GetIm()
+
+        }
+
+        // subtract specified complex number from current complex number
+        public void Subtract(ComplexNumber other)
+        {
+            // ** COMPLETE ** -- Requires use of GetRe(), GetIm()
+        }
+
+        // multiply current complex number by specified complex number
+        public void Multiply(ComplexNumber other)
+        {
+            // save real part of original complex numner
+            double x = this.re;
+
+            // ** COMPLETE ** -- Requires use of GetRe(), GetIm()
+        }
+
+        // divide current complex number by specified complex number
+        // Divisor must be <> 0
+        public bool Divide(ComplexNumber other)
+        {
+            // save real part of original complex numner
+            double x = this.re;
+
+            double divisor = (other.GetRe() * other.GetRe()) +
+                                (other.GetIm() * other.GetIm());
+
+            // check if division is possible
+            if (divisor != 0.0)
+            {
+                // ** COMPLETE ** -- Requires use of GetRe(), GetIm()
+                return true;
+            }
+            else
+                return false;
+        }
+
+        // return conjugate of current complex number
+        public ComplexNumber Conjugate()
+        {
+            return new ComplexNumber(this.re, -this.im);
+        }
+
+        // return a string representing the current complex number
+        public string ToString()
+        {
+            string sign = (this.im < 0 ? " - " : " + ");
+
+            return this.re + sign + Math.Abs(this.im) + "i";
+        }
+
+    }  // end class
+} // end namespace
---
+++ ComplexProgram.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ComplexNumberApplication
+{
+    class ComplexProgram
+    {
+        private const string EXIT = "e";
+
+        // Class method. Since no instances of the program class are usually created,
+        // the method is used only by Main(). To declare a class method, use the
+        // keyword static.
+        public static ComplexNumber ReadComplex(string prompt)
+        {
+            Console.WriteLine(prompt);
+            Console.Write("Real part => ");
+            double r = double.Parse(Console.ReadLine());
+
+            Console.Write("Imaginary => ");
+            double i = double.Parse(Console.ReadLine());
+
+            return new ComplexNumber(r, i);
+
+        } // end ReadComplex
+
+        static void Main()
+        {
+
+            ComplexNumber c1, c2;
+            string op;
+
+            // Read the first complex number
+            c1 = ReadComplex("\nPlease enter the first complex number:");
+
+            // Read in the operation required
+            do
+            {
+
+                Console.Write("\nEnter arithmetic operation    + - * / e<xit> => ");
+                op = Console.ReadLine().ToLower();
+
+                if (op.Equals(EXIT))
+                    break;
+
+                else if (op.Equals("+"))
+                {
+                    c2 = ReadComplex("\nEnter second complex number:");
+                    Console.Write("\nAdding " + c1.ToString() + " to " + c2.ToString() + " results in: ");
+                    c1.Add(c2);
+                    Console.Write(c1.ToString());
+                }
+                else if (op.Equals("-"))
+                {
+                    c2 = ReadComplex("\nEnter second complex number:");
+                    Console.Write("\nSubtracting " + c2.ToString() + " from " + c1.ToString() + " results in: ");
+                    c1.Subtract(c2);
+                    Console.Write(c1.ToString());
+                }
+                else if (op.Equals("*"))
+                {
+                    c2 = ReadComplex("\nEnter second complex number:");
+                    Console.Write("\nMultiplying " + c1.ToString() + " by " + c2.ToString() + " results in: ");
+                    c1.Multiply(c2);
+                    Console.Write(c1.ToString());
+                }
+                else if (op.Equals("/"))
+                {
+                    c2 = ReadComplex("\nEnter second complex number:");
+                    if (c1.Divide(c2))
+                    {
+                        Console.Write("\nDividing " + c1.ToString() + " by " + c2.ToString() + " results in: ");
+                        Console.Write(c1.ToString());
+                    }
+                    else
+                        Console.WriteLine("\nUnable to divide " + c1.ToString() + " by " + c2.ToString() + ".");
+                }
+
+
+            } while (!op.Equals(EXIT)); // end do while
+
+            Console.WriteLine("\nPress RETURN to exit program.");
+            Console.ReadLine();
+        } // end Main()
+    } // end class
+} // end namespace
